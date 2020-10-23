eb5abc24eb9e9c19c30f88648e5c020a94d79fb5
kevinsulatra <ksulatra@gmail.com>
2020-10-23 09:07:57 +0100

    Revert "Read and Write to database."
    This reverts commit ec1c18291f19df810db28c7997206cf80e9271ab.

--- Front-End/Manager.cs
+++ Front-End/Manager.cs
@@ -46,16 +46,20 @@ namespace Front_End
             {
                 //open form for new user.
 
-            }
-            else if (usernametxt.Text == "admin")
-            {
+            }else if (usernametxt.Text == "admin"){
                 Business_Operations BusinessForm = new Business_Operations();
                 Back_End.Program LoginAuthenitcate = new Back_End.Program(usernametxt.Text, passwordtxt.Text, BusinessForm);
                 this.Hide();
             }
+          
+           
+            
+            
             //this.Hide();
             //Business_Operations b = new Business_Operations();
             //b.ShowDialog();
+            
         }
+
     }
 }
